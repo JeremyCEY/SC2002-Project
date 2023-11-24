c3059af50759131b901eb25eca1b8b0743979e71
tyt1130 <tangyutong0306@gmail.com>
2023-11-24 12:06:58 +0800

    add information needed to display

--- src/main/model/request/Suggestion.java
+++ src/main/model/request/Suggestion.java
@@ -248,6 +248,18 @@ public class Suggestion implements Request{
 			if (! getLocation().equals(null)) {
 				result.append(String.format("| Location                  | %-21s |\n", getLocation()));
 			}
+			if (getTotalSlots() != -1){
+				result.append(String.format("| Camp Attendee Total slots | ") + getTotalSlots());
+			}
+			if (getCampCommSlots() != -1){
+				result.append(String.format("| Camp Commitee Total slots |") + getCampCommSlots());
+			}
+			if (! getDescription().equals(null)){
+				result.append(String.format("| Camp Description | %-21s |", getDescription()));
+			}
+			if (! getCampStaff().equals(null)){
+				result.append(String.format("| Camp Staff id in charge | %-21s |", getCampStaff()));
+			}
 			// Add similar if statements for other fields
 		
 			return result.toString();
