77f9851f6ac0867187798246ed556f6570560055
tyt1130 <tangyutong0306@gmail.com>
2023-11-26 13:23:45 +0800

    Update ViewUserProfile.java

--- src/main/boundary/account/ViewUserProfile.java
+++ src/main/boundary/account/ViewUserProfile.java
@@ -1,11 +1,3 @@
-/*
- * @Author: tyt1130 tangyutong0306@gmail.com
- * @Date: 2023-11-09 11:03:36
- * @LastEditors: tyt1130 tangyutong0306@gmail.com
- * @LastEditTime: 2023-11-26 13:22:48
- * @FilePath: \SC2002-Project\src\main\boundary\account\ViewUserProfile.java
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 /**
  * The main.boundary.account package contains classes responsible for providing user interfaces
  * related to user account actions, including viewing user profiles.
@@ -98,3 +90,4 @@ public class ViewUserProfile {
         throw new PageBackException();
     }
 }
+
